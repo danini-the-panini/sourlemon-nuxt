b422cae78be13bc63eacf6af8787ff938680b531
Daniel Smith <jellymann@gmail.com>
2020-09-24 13:10:02 +0200

    add: blog timestamps

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -3,12 +3,21 @@
     async asyncData({ $content, params }) {
       const article = await $content('articles', params.slug).fetch()
       return { article }
+    },
+    methods: {
+      formatDate(date) {
+        const options = { year: 'numeric', month: 'long', day: 'numeric' }
+        return new Date(date).toLocaleDateString('en', options)
+      }
     }
   }
 </script>
 
 <template>
   <article>
+    <!-- <pre> {{ article }} </pre> -->
+    <p>Post created: {{ formatDate(article.createdAt) }}</p>
+    <p>Post last updated: {{ formatDate(article.updatedAt) }}</p>
     <nuxt-content :document="article" />
   </article>
 </template>
