89efaadd051a55e5d26a682cc61629d064224e28
Daniel Smith <jellymann@gmail.com>
2020-09-24 14:15:58 +0200

    add: layout with nav

---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1><a href="/">Sour <code>{&gt;.&lt;}</code> Lemon</a></h1>
+    
+    <nav>
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/blog/">Blog</a></li>
+      </ul>
+    </nav>
+    <nuxt />
+  </div>
+</template>
