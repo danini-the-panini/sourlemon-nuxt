289fb80911fa56da75a24b5ff0b2e20daff63f7c
Daniel Smith <jellymann@gmail.com>
2020-09-24 17:18:27 +0200

    add: content and styling to home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,36 @@
 <template>
   <div>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-    </p>
+    <div class="hero bg-banner">
+      <div class="hero-body p-relative">
+        <figure class="avatar avatar-xl">
+          <img src="~assets/me.png" alt="My Profile Picture">
+        </figure>
+        <h1>About Me</h1>
+      </div>
+    </div>
+    <p>Hi, I'm Dan. Nice to meet you.</p>
+
+    <p>I love my job. I'm a full-time full-stack web developer at <a href="http://www.platform45.com/">Platform45</a>.</p>
+
+    <p>This website is a collection of technology-related things I have decided to share with the world. Enjoy :)</p>
   </div>
 </template>
+
+<style scoped lang="scss">
+.bg-banner {
+  position: relative;
+  background-image: url('~assets/banner.jpg');
+  background-position: center;
+  background-size: cover;
+
+  &::before {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(255, 255, 255, 0.8);
+  }
+}
+</style>
